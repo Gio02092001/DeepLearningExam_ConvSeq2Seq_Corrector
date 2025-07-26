d611d2d5450ebc29f08f1d4cea272a10d0440d89
root <giovannicarlucci9@yahoo.de>
2025-07-26 16:54:44 +0000

    Final Fix: Load PreTrained Model

--- main.py
+++ main.py
@@ -96,8 +96,8 @@ def main():
     if hasattr(args, 'pretrained') and args.pretrained is not None:
         # args.file esiste e non è None
         pretrained = args.pretrained
-        ckpt = torch.load(f"/models/{pretrained}/best_model.pt", map_location=device)
-        config = load_parameters("/models/pretrained/config.yaml")
+        ckpt = torch.load(f"models/{pretrained}/best_model.pt", map_location=device)
+        config = load_parameters(f"models/{pretrained}/config.yaml")
         # fai qualcosa con file_path
     else:
         config = load_parameters()
